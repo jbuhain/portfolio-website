5c87d8d3381b684de2db7854063b1444981ca0f6
Joshua Buhain <jbuhain1@gmail.com>
2023-11-07 11:37:19 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Intro from './components/Intro.jsx';
+import Contacts from './components/Contacts.jsx';
+import Footer from './components/Footer.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Timeline from './components/Timeline.jsx';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Intro />
+      <Contacts />
+      <Footer />
+      <Portfolio />
+      <Timeline />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Timeline.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Timeline() {
+    return (
+        <div>Timeline</div>
+    )
+        
+}
+
+export default Timeline;
