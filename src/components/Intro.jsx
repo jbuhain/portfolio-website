9a4ec0b1e33bfe61b74b6fdea82617a2c4e43ae2
Joshua Buhain <jbuhain1@gmail.com>
2023-11-07 14:47:46 -0800

    added portfolio grid (rough)

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Intro() {
     return (
-        <div>Intro</div>
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Joshua Buhain</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">add the bio here</p>
+        </div>
     )
         
 }
---
+++ src/data/portfolio.js
@@ -0,0 +1,10 @@
+// exports an array of object, where each object represents the project
+
+export default [
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://joshuabuhain.dev',
+    }
+]
