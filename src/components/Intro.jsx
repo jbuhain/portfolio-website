1285a3d3cee55f7dac44c96d9e7271cd34491f7a
Joshua Buhain <jbuhain1@gmail.com>
2023-11-09 21:14:17 -0800

    Decluttered

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -15,14 +15,14 @@ function Intro() {
                 wrapper="span"
                 speed={10}
                 cursor={false}
-                className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold font-pixelify"
+                className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold"
                 repeat={Infinity}
             />
             {/* <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Joshua Buhain</h1> */}
             <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">Hello I’m Josh, a new grad with a Bachelor’s in Computer Science from UC Davis. I am currently on the job hunt and am excited to find opportunities in the realm of Software Engineering. 
-
-What I lack in experience I make up for with personal projects and certifications. I also hold internship experience as a developer.</p>
+            {/* <p className="text-sm max-w-xl mb-6 font-bold">
+                Hello I’m Josh, a new grad with a Bachelor’s in Computer Science from UC Davis. I am currently on the job hunt and am excited to find opportunities in the realm of Software Engineering. What I lack in experience I make up for with personal projects and certifications. I also hold internship experience as a developer.
+            </p> */}
         </div>
     )
         
--- src/data/timeline.js
+++ src/data/timeline.js
@@ -3,24 +3,24 @@ export default [
         date: 'Current',
         title: '2x AWS Certified',
         duration: '',
-        details: 'details here',
+        details: 'Certifications obtained were the AWS Certified Solutions Architect Associate and AWS Certified Cloud Practitioner.',
     },
     {
         date: '2023',
         title: "Bachelor's Degree in Computer Science",
         duration: '4 years',
-        details: 'details here'
+        details: "Relevant Coursework include Software Engineering Methodologies, Data Structures and Algorithms, Machine Learning, Artificial Intelligence, Web Programming, and Business Writing."
     },
     {
-        date: '2020',
-        title: 'Intern Developer',
+        date: '2019-2020',
+        title: 'Intern Developer @ CK-12 Foundation',
         duration: '8 months, 40 hrs/week',
-        details: 'details here'
+        details: "As an intern, I developed interactive learning tools that were embedded in CK-12's digital textbooks using JavaScript and a math scripting language called GeoGebra."
     },
     {
         date: '2018',
         title: 'Early Coding Years',
         duration: '4 years',
-        details: 'details here'
+        details: "In high school, I was an AP Computer Science Teaching Assistant and a Coding Competition Finalist."
     }
 ]
