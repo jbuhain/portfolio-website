fee094f300169884f73ed378b51d0cb75a2ec230
Joshua Buhain <jbuhain1@gmail.com>
2023-11-10 10:44:51 -0800

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -19,7 +19,14 @@ function Intro() {
                 repeat={Infinity}
             />
             {/* <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Joshua Buhain</h1> */}
-            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+            <p className="text-base md:text-xl mb-3 font-medium">Computer Science @ UC Davis</p>
+            <p>
+                <a href="https://www.linkedin.com/in/jbuhain/" target="_blank" className="text-blue-500">LinkedIn</a>
+                {' '}|{' '}
+                <a href="https://github.com/jbuhain" target="_blank" className="text-blue-500">GitHub</a>
+            </p>
+
+
             {/* <p className="text-sm max-w-xl mb-6 font-bold">
                 Hello I’m Josh, a new grad with a Bachelor’s in Computer Science from UC Davis. I am currently on the job hunt and am excited to find opportunities in the realm of Software Engineering. What I lack in experience I make up for with personal projects and certifications. I also hold internship experience as a developer.
             </p> */}
